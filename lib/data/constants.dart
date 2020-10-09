eccb179e15972b931957f4b60941f825ddab43f3
ukkey0518 <ukitto0518@gmail.com>
2020-10-09 20:02:05 +0900

    ステージを表す定数を定義

---
+++ lib/data/constants.dart
@@ -0,0 +1,17 @@
+// ステージ
+enum Stage {
+  // 孤島
+  ISLE_OF_DAWN,
+  // 草原
+  DAYLIGHT_PRAIRIE,
+  // 雨林
+  HIDDEN_FOREST,
+  // 峡谷
+  VALLEY_OF_TRIUMPH,
+  // 捨てられた地
+  GOLDEN_WASTELAND,
+  // 書庫
+  VAULT_OF_KNOWLEDGE,
+  // 暴風域
+  STORM,
+}
