98b4accb19ee0ba99cdd9399d1146b9d0b634ee2
ukkey0518 <ukitto0518@gmail.com>
2020-10-09 21:23:10 +0900

    ItemDataの構成を変更

--- lib/data/classes/data_classes/item_data.dart
+++ lib/data/classes/data_classes/item_data.dart
@@ -4,76 +4,49 @@ import 'package:sky_util_app/data/constants.dart';
 class ItemData {
   const ItemData({
     @required this.id,
-    @required this.itemTypeIndex,
-    @required this.name,
-    @required this.payTypeIndex,
+    @required this.itemType,
+    @required this.payType,
     @required this.price,
     @required this.imagePath,
   });
 
-  ItemData.magic({
+  const ItemData.normal({
     @required this.id,
-    @required this.payTypeIndex,
-    @required bool isSmall,
+    @required this.itemType,
     @required this.price,
     @required this.imagePath,
-  })  : this.name = '魔法(${isSmall ? '小' : '大'})',
-        this.itemTypeIndex = ItemType.MAGIC.index;
+  }) : this.payType = PayType.NORMAL_CANDLE;
 
-  ItemData.outfit({
+  const ItemData.star({
     @required this.id,
-    @required this.name,
-    @required this.payTypeIndex,
+    @required this.itemType,
     @required this.price,
     @required this.imagePath,
-  }) : this.itemTypeIndex = ItemType.OUTFIT.index;
+  }) : this.payType = PayType.STAR_CANDLE;
 
-  ItemData.expression({
+  const ItemData.heart({
     @required this.id,
-    @required this.name,
-    @required this.payTypeIndex,
+    @required this.itemType,
     @required this.price,
     @required this.imagePath,
-  }) : this.itemTypeIndex = ItemType.EXPRESSION.index;
-
-  ItemData.heart({
-    @required this.id,
-    @required this.price,
-  })  : this.name = 'ハート',
-        this.payTypeIndex = PayType.NORMAL_CANDLE.index,
-        this.itemTypeIndex = ItemType.MAGIC.index,
-        //TODO: 画像パス指定
-        this.imagePath = null;
-
-  ItemData.lock({
-    @required this.id,
-    @required this.price,
-  })  : this.name = 'アンロック',
-        this.itemTypeIndex = ItemType.MAGIC.index,
-        this.payTypeIndex = PayType.STAR_CANDLE.index,
-        //TODO: 画像パス指定
-        this.imagePath = null;
+  }) : this.payType = PayType.HEART;
 
   final String id;
-  final int itemTypeIndex;
-  final String name;
-  final int payTypeIndex;
+  final ItemType itemType;
+  final PayType payType;
   final int price;
   final String imagePath;
 
   ItemData copyWith({
     String id,
-    int itemTypeIndex,
-    String name,
-    int payTypeIndex,
+    ItemType itemType,
+    PayType payType,
     int price,
     String imagePath,
   }) {
     if ((id == null || identical(id, this.id)) &&
-        (itemTypeIndex == null ||
-            identical(itemTypeIndex, this.itemTypeIndex)) &&
-        (name == null || identical(name, this.name)) &&
-        (payTypeIndex == null || identical(payTypeIndex, this.payTypeIndex)) &&
+        (itemType == null || identical(itemType, this.itemType)) &&
+        (payType == null || identical(payType, this.payType)) &&
         (price == null || identical(price, this.price)) &&
         (imagePath == null || identical(imagePath, this.imagePath))) {
       return this;
@@ -81,9 +54,8 @@ class ItemData {
 
     return new ItemData(
       id: id ?? this.id,
-      itemTypeIndex: itemTypeIndex ?? this.itemTypeIndex,
-      name: name ?? this.name,
-      payTypeIndex: payTypeIndex ?? this.payTypeIndex,
+      itemType: itemType ?? this.itemType,
+      payType: payType ?? this.payType,
       price: price ?? this.price,
       imagePath: imagePath ?? this.imagePath,
     );
@@ -91,7 +63,7 @@ class ItemData {
 
   @override
   String toString() {
-    return 'ItemData{id: $id, itemTypeIndex: $itemTypeIndex, name: $name, payTypeIndex: $payTypeIndex, price: $price, imagePath: $imagePath}';
+    return 'ItemData{id: $id, itemType: $itemType, payType: $payType, price: $price, imagePath: $imagePath}';
   }
 
   @override
@@ -100,27 +72,24 @@ class ItemData {
       (other is ItemData &&
           runtimeType == other.runtimeType &&
           id == other.id &&
-          itemTypeIndex == other.itemTypeIndex &&
-          name == other.name &&
-          payTypeIndex == other.payTypeIndex &&
+          itemType == other.itemType &&
+          payType == other.payType &&
           price == other.price &&
           imagePath == other.imagePath);
 
   @override
   int get hashCode =>
       id.hashCode ^
-      itemTypeIndex.hashCode ^
-      name.hashCode ^
-      payTypeIndex.hashCode ^
+      itemType.hashCode ^
+      payType.hashCode ^
       price.hashCode ^
       imagePath.hashCode;
 
   factory ItemData.fromMap(Map<String, dynamic> map) {
     return new ItemData(
       id: map['id'] as String,
-      itemTypeIndex: map['itemTypeIndex'] as int,
-      name: map['name'] as String,
-      payTypeIndex: map['payTypeIndex'] as int,
+      itemType: map['itemType'] as ItemType,
+      payType: map['payType'] as PayType,
       price: map['price'] as int,
       imagePath: map['imagePath'] as String,
     );
@@ -130,9 +99,8 @@ class ItemData {
     // ignore: unnecessary_cast
     return {
       'id': this.id,
-      'itemTypeIndex': this.itemTypeIndex,
-      'name': this.name,
-      'payTypeIndex': this.payTypeIndex,
+      'itemType': this.itemType,
+      'payType': this.payType,
       'price': this.price,
       'imagePath': this.imagePath,
     } as Map<String, dynamic>;
