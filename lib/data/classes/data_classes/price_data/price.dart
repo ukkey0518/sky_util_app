d98a1095502c28738c43ec1375b33b91bdcf7577
ukkey0518 <ukitto0518@gmail.com>
2020-10-10 18:41:54 +0900

    PriceDataのデフォルトコンスタラクタを削除＆toString()のオーバーライド

--- lib/data/classes/data_classes/price_data/price.dart
+++ lib/data/classes/data_classes/price_data/price.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sky_util_app/data/constants.dart';
 
 class Price {
-  const Price({
-    @required this.payType,
-    @required this.price,
-  });
-
   const Price.free()
       : this.payType = PayType.FREE,
         this.price = 0;
@@ -19,4 +14,24 @@ class Price {
 
   final PayType payType;
   final int price;
+
+  @override
+  String toString() {
+    var payTypeText;
+    switch (payType) {
+      case PayType.FREE:
+        payTypeText = '無料';
+        break;
+      case PayType.NORMAL_CANDLE:
+        payTypeText = '通常キャンドル';
+        break;
+      case PayType.STAR_CANDLE:
+        payTypeText = '星キャンドル';
+        break;
+      case PayType.HEART:
+        payTypeText = 'ハート';
+        break;
+    }
+    return 'Price{$payTypeText,${price == null || price == 0 ? '' : price}';
+  }
 }
