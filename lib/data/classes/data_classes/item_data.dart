835c2ee2ac044b16545d111f1d416d8febedc591
ukkey0518 <ukitto0518@gmail.com>
2020-10-10 17:14:13 +0900

    ItemDataのコンストラクタを変更(ItemType別)

--- lib/data/classes/data_classes/item_data.dart
+++ lib/data/classes/data_classes/item_data.dart
@@ -10,33 +10,124 @@ class ItemData {
     @required this.imagePath,
   });
 
-  const ItemData.free({
+  const ItemData.magicS({
     @required this.id,
-    @required this.itemType,
-    @required this.imagePath,
-  })  : this.payType = PayType.FREE,
-        this.price = 0;
-
-  const ItemData.normal({
-    @required this.id,
-    @required this.itemType,
+    @required this.payType,
     @required this.price,
     @required this.imagePath,
-  }) : this.payType = PayType.NORMAL_CANDLE;
+  }) : this.itemType = ItemType.MAGIC_SMALL;
 
-  const ItemData.star({
+  const ItemData.magicB({
     @required this.id,
-    @required this.itemType,
+    @required this.payType,
     @required this.price,
     @required this.imagePath,
-  }) : this.payType = PayType.STAR_CANDLE;
+  }) : this.itemType = ItemType.MAGIC_BIG;
+
+  const ItemData.pants({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.OUTFIT_PANTS;
+
+  const ItemData.mask({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.OUTFIT_MASK;
+
+  const ItemData.hair({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.OUTFIT_HAIR;
+
+  const ItemData.cape({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.OUTFIT_CAPE;
+
+  const ItemData.item({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.OUTFIT_ITEM;
+
+  const ItemData.exp1({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_1;
+
+  const ItemData.exp2({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_2;
+
+  const ItemData.exp3({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_3;
+
+  const ItemData.exp4({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_4;
+
+  const ItemData.exp5({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_5;
+
+  const ItemData.exp6({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.EXPRESSION_6;
+
+  const ItemData.call({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.CALL;
+
+  const ItemData.score({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.SCORE;
 
   const ItemData.heart({
     @required this.id,
-    @required this.itemType,
+    @required this.payType,
     @required this.price,
     @required this.imagePath,
-  }) : this.payType = PayType.HEART;
+  }) : this.itemType = ItemType.HEART;
+
+  const ItemData.unlock({
+    @required this.id,
+    @required this.payType,
+    @required this.price,
+    @required this.imagePath,
+  }) : this.itemType = ItemType.TREE_UNLOCK;
 
   final String id;
   final ItemType itemType;
