f6d0bda4b433b148973ef2c3ded5282bc6714cbb
ukkey0518 <ukitto0518@gmail.com>
2020-11-05 18:27:17 +0900

    ホーム画面の作成

--- lib/app/views/home/home.dart
+++ lib/app/views/home/home.dart
@@ -4,7 +4,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: Text('ホーム'),
+        ),
+      ),
     );
   }
 }
