47cfa6049100f72332fa6fd0ff8043b93816bf99
ukkey0518 <ukitto0518@gmail.com>
2020-11-05 17:41:24 +0900

    画像パスクラスの作成

---
+++ lib/data/image_path.dart
@@ -0,0 +1,19 @@
+class ImagePath {
+  ImagePath._();
+
+  static const ic_heart = 'assets/images/ic/common/heart_ic.png';
+  static const ic_magic_big = 'assets/images/ic/common/magic_big_ic.png';
+  static const ic_magic_small = 'assets/images/ic/common/magic_small_ic.png';
+  static const ic_score = 'assets/images/ic/common/score_ic.png';
+  static const ic_tree_unlock = 'assets/images/ic/common/tree_unlock_ic.png';
+
+  static const ic_1_1_exp = 'assets/images/ic/1_1_exp.png';
+  static const ic_1_1_of_h = 'assets/images/ic/1_1_of_h.png';
+  static const ic_1_1_of_p = 'assets/images/ic/1_1_of_p.png';
+  static const ic_1_2_exp = 'assets/images/ic/1_2_exp.png';
+  static const ic_1_2_of_h = 'assets/images/ic/1_2_of_h.png';
+  static const ic_1_2_of_p = 'assets/images/ic/1_2_of_p.png';
+  static const ic_1_3_exp = 'assets/images/ic/1_3_exp.png';
+  static const ic_1_3_of_h = 'assets/images/ic/1_3_of_h.png';
+  static const ic_1_3_of_m = 'assets/images/ic/1_3_of_m.png';
+}
