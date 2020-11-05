a2b5b6a99abce431ff5b23a34daac74f9700b070
ukkey0518 <ukitto0518@gmail.com>
2020-11-05 18:37:27 +0900

    デフォルトフォントの指定

--- lib/data/theme/theme_data.dart
+++ lib/data/theme/theme_data.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sky_util_app/data/constants.dart';
 
 ThemeData themeData(BuildContext context) {
   return ThemeData(
-      // brightness: Brightness.dark,
-      );
+    // brightness: Brightness.dark,
+    fontFamily: fontPixel,
+  );
 }
